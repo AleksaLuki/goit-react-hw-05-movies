0b16a2ecd6dd1a25ac70b7ae0898a82944e7490d
AleksaLuki <dimirtiy9000@gmail.com>
2023-05-16 09:33:12 +0300

    new file

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,19 +1,19 @@
-import { Route, Routes } from "react-router-dom";
-import { lazy, Suspense } from 'react';
+// import { Route, Routes } from "react-router-dom";
+// import { lazy, Suspense } from 'react';
 
-// export const App = () => {
-//   return (
-//     <div
-//       style={{
-//         height: '100vh',
-//         display: 'flex',
-//         justifyContent: 'center',
-//         alignItems: 'center',
-//         fontSize: 40,
-//         color: '#010101'
-//       }}
-//     >
-//       React homework template
-//     </div>
-//   );
-// };
+export const App = () => {
+  return (
+    <div
+      style={{
+        height: '100vh',
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        fontSize: 40,
+        color: '#010101'
+      }}
+    >
+      React homework template
+    </div>
+  );
+};
